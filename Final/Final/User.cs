27379b14ea785464fe3bd5dc5e362b7a53813e4b
Matt Swaffer <maswaffer@gmail.com>
2017-03-14 21:00:11 -0600

    Gitignore

--- Final/Final/User.cs
+++ Final/Final/User.cs
@@ -25,7 +25,17 @@ namespace Final
             set
             {
                 //if fname doesnt contain numbers or symbols
-                _fname = value;
+                /*
+                 * MS see comment below
+                 */
+
+                if (value.All(char.IsLetter))
+                {
+                    _fname = value;
+                }else
+                {
+                    throw new InvalidNameException("Name cannot contain numbers");
+                }
             }
         }
 
@@ -40,7 +50,35 @@ namespace Final
         }
 
         //are either of these setups correct for professional coding? or should all of this be done through database?
-
+        /*
+         * MS
+         * I think what you are asking is: should we be validating things before sending to the database. 
+         * 
+         * The answer generally is "yes". A lot of that depends on your architecture but it's pretty rare to put all of your validation
+         * in the database. Mostly this is because the cost (in CPU cycles and bandwidth) is too high to push things all the way to the DB
+         * only to find out it is invalid.
+         * 
+         * So what you are doing here makes sense... and yes, you are correctly checking things inside the code block for the set{} 
+         * Some programmers would prefer to make actual validation classes and tap into those at the UI level. The way you are doing 
+         * it here you would probably need to throw an exception if validation failed. Philosophically, some programmers
+         * are very averse to using Exceptions for anything other than failures of the system. Many however see exceptions as
+         * a perfectly valid method of returning information from a method (or property set). 
+         * 
+         * Considering that Microsoft allows you to extend the Exception class to make your own, I have seen this used 
+         * effectively to pass information back.  For instance, here you could create your own exception and throw it in the code above.  
+         * 
+         * (I put an example in the code)
+         */
         
+    }
+
+    /*
+     * MS
+     * See this link: https://msdn.microsoft.com/en-us/library/87cdya3t(v=vs.110).aspx
+     * In practice you would put this in it's own file.  Sometimes we put all of our custom exceptions in a file
+     * named "CustomExceptions.cs" but practice varies on that. 
+     */
+    public class InvalidNameException : Exception {
+        public InvalidNameException(string message) : base(message) { }
     }
 }
